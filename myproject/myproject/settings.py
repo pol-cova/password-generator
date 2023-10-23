918d7ab23434c70be3322d285705bdbd1998152e
pol-cova <83374367+pol-cova@users.noreply.github.com>
2023-10-23 15:55:55 -0600

    Updated: Preapare for deploy

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-t0926zu0^e)091z)85@67&wol(1%hj23-vr2@#j9)!3ls#4+zc"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
