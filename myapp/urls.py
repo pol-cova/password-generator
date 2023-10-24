835a7284e046b58d085bfa263858c2d8df9531d0
pol-cova <83374367+pol-cova@users.noreply.github.com>
2023-10-23 20:45:09 -0600

    Fix: Resolving production bugs

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 from django.conf import settings
-from django.conf.urls import static
+from django.conf.urls.static import static
 
 # URLS
 urlpatterns = [
@@ -11,5 +11,5 @@ urlpatterns = [
     path('generate/', views.generate_password, name='generate_password'),
 ]
 
-
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
