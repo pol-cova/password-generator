72c1173aedb3e0b36974d6dff3194a6238731eb4
pol-cova <83374367+pol-cova@users.noreply.github.com>
2023-10-23 20:34:23 -0600

    Fixing: Fix bug in production

--- myapp/urls.py
+++ myapp/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('generate/', views.generate_password, name='generate_password'),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
